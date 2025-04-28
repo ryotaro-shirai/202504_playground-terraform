79d4e16bc2d0e8a4a6658114c1a609075f522f75
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-04-28 16:16:07 +0900

    Add Cloudfront

--- terraform/_shared/locals.tf
+++ terraform/_shared/locals.tf
@@ -1,3 +1,4 @@
 locals {
   aws_resource_name_prefix = "${var.env_prefix}-playground-terraform"
+  s3_origin_id = "myS3Origin"
 }
--- terraform/_shared/s3.tf
+++ terraform/_shared/s3.tf
@@ -6,3 +6,26 @@ resource "aws_s3_bucket" "s3_bucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_policy" "s3_bucket" {
+    bucket = aws_s3_bucket.s3_bucket.id
+    policy = data.aws_iam_policy_document.allow-cloudfront.json
+}
+
+data "aws_iam_policy_document" "allow-cloudfront" {
+  statement {
+    sid = "Allow CloudFront"
+    effect = "Allow"
+    principals {
+        type = "AWS"
+        identifiers = [aws_cloudfront_origin_access_identity.playground-cloudfront.iam_arn]
+    }
+    actions = [
+        "s3:GetObject"
+    ]
+
+    resources = [
+        "${aws_s3_bucket.s3_bucket.arn}/*"
+    ]
+  }
+}
